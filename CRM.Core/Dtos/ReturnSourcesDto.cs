c61d6e654d823b2920bc98761dfc11e5b116d550
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-03-03 17:16:37 +0200

    Update ReturnSourceDto

--- CRM.Core/Dtos/ReturnSourcesDto.cs
+++ CRM.Core/Dtos/ReturnSourcesDto.cs
@@ -2,14 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CRM.Core.Dtos
 {
     public class ReturnSourcesDto
     {
-        public string Message { get; set; }
+        [JsonIgnore]
         public bool IsSuccess { get; set; }
+        [JsonIgnore(Condition =JsonIgnoreCondition.WhenWritingNull)]
+        public string[] Errors { get; set; }
+        [JsonIgnore(Condition =JsonIgnoreCondition.WhenWritingNull)]
         public IList<SourceDto> Sources { get; set; }
     }
 }
