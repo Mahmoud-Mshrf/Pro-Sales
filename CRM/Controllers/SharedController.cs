2f3bdb15461d8b872d109a20dcda720c8896060e
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-03-03 14:44:14 +0200

    Add GetAllInterests and  GetAllSources Endpoints to SharedController

--- CRM/Controllers/SharedController.cs
+++ CRM/Controllers/SharedController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CRM.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CRM.Controllers
@@ -7,5 +8,30 @@ namespace CRM.Controllers
     [ApiController]
     public class SharedController : ControllerBase
     {
+        private readonly ISharedService _sharedService;
+        public SharedController(ISharedService sharedService)
+        {
+            _sharedService = sharedService;
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllInterests()
+        {
+            var result = await _sharedService.GetAllInterests();
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result.Interests);
+        }
+        [HttpGet("[action]")]
+        public async Task<IActionResult> GetAllSources()
+        {
+            var result = await _sharedService.GetAllSources();
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result.Sources);
+        }
     }
 }
