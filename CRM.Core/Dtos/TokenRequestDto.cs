9e8233ed24c70366aa6dbe1949ec90365a70e367
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-03-03 11:52:09 +0200

    Apply ValidationErrorOrderAttribute on TokenRequestDto

--- CRM.Core/Dtos/TokenRequestDto.cs
+++ CRM.Core/Dtos/TokenRequestDto.cs
@@ -1,11 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CRM.Core.Custom_Attributes;
+using System.ComponentModel.DataAnnotations;
 
 namespace CRM.Core.Dtos
 {
     public class TokenRequestDto
     {
+        [ValidationErrorOrder(1)]
         [Required, EmailAddress]
         public string Email { get; set; }
+        [ValidationErrorOrder(2)]
         [Required]
         public string Password { get; set; }
     }
