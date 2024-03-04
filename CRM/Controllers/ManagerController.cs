0e44b7e29d98ec7f6e7ffed905842ae136019a57
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-03-05 01:11:02 +0200

    Add ManagerController, add-interest and add-source endpoints

---
+++ CRM/Controllers/ManagerController.cs
@@ -0,0 +1,39 @@
+﻿using CRM.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagerController : ControllerBase
+    {
+        private readonly IManagerService _managerService;
+
+        public ManagerController(IManagerService managerService)
+        {
+            _managerService = managerService;
+        }
+
+        [HttpPost("add-interest")]
+        public async Task<IActionResult> AddInterest([FromForm] string name)
+        {
+            var result = await _managerService.AddInterest(name);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+        [HttpPost("add-source")]
+        public async Task<IActionResult> AddSource([FromForm] string name)
+        {
+            var result = await _managerService.AddSource(name);
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result.Message);
+            }
+            return Ok(result);
+        }
+    }
+}
