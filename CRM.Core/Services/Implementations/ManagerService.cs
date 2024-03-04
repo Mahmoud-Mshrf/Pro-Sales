5b6de4e582f17f87875219a1a6e9bee3e43e48a2
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-03-05 01:02:14 +0200

    Add IManagerService and its implementation

---
+++ CRM.Core/Services/Implementations/ManagerService.cs
@@ -0,0 +1,13 @@
+﻿using CRM.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Core.Services.Implementations
+{
+    public class ManagerService:IManagerService
+    {
+    }
+}
