048fc6f182b14b6aa85683ac0e810179e129d73c
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-03-05 01:04:53 +0200

    Add AddSource function to IManagerService

--- CRM.Core/Services/Implementations/ManagerService.cs
+++ CRM.Core/Services/Implementations/ManagerService.cs
@@ -1,4 +1,6 @@
-﻿using CRM.Core.Services.Interfaces;
+﻿using CRM.Core.Dtos;
+using CRM.Core.Models;
+using CRM.Core.Services.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,36 @@ namespace CRM.Core.Services.Implementations
 {
     public class ManagerService:IManagerService
     {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ManagerService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<ResultDto> AddSource(string name)
+        {
+
+            var interest = new Source
+            {
+                SourceName = name
+            };
+            var sources = await _unitOfWork.Sources.GetAllAsync();
+            if (sources.Any(sources => sources.SourceName == name))
+            {
+                return new ResultDto
+                {
+                    IsSuccess = false,
+                    Message = "Source already exists"
+                };
+            }
+            var result = await _unitOfWork.Sources.AddAsync(interest);
+            _unitOfWork.complete();
+            return new ResultDto
+            {
+                IsSuccess = true,
+                Message = "Source added successfully"
+            };
+        }
     }
 }
--- CRM.Core/Services/Interfaces/IManagerService.cs
+++ CRM.Core/Services/Interfaces/IManagerService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CRM.Core.Dtos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace CRM.Core.Services.Interfaces
 {
     public interface IManagerService
     {
+        Task<ResultDto> AddSource(string name);
     }
 }
