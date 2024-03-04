bcb0c907561db0831103b0e13dd045d35ecaa7ba
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-03-04 21:42:34 +0200

    Add ReturnCustomerDto

---
+++ CRM.Core/Dtos/ReturnCustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CRM.Core.Dtos
+{
+    public class ReturnCustomerDto : CustomerDto
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
+        public string[] Errors { get; set; }
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        public int CustomerId { get; set; }
+        [JsonIgnore]
+        public bool IsSuccess { get; set; }
+
+        public ReturnCustomerDto()
+        {
+            Errors = null;
+        }
+    }
+}
