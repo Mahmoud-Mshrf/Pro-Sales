c6dd08bcae969ce7d7d154b02bc0b2a22f9bbc8b
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-03-05 01:23:47 +0200

    Add RoleModel and UserRolesDto to Dtos

---
+++ CRM.Core/Dtos/UserRolesDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Core.Dtos
+{
+    public class UserRolesDTO
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public IEnumerable<RoleModel> Roles { get; set; }
+        public string ErrorMessage { get; set; }
+    }
+}
