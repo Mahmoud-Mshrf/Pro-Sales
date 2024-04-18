7d47933408fd3c553f39f1d0815252216ae103da
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-04-18 13:36:44 +0200

    add IsDisabled property to Interest class

---
+++ CRM.Infrastructure/Migrations/20240418112530_InterestDisable.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRM.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InterestDisable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDisabled",
+                table: "Interests",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDisabled",
+                table: "Interests");
+        }
+    }
+}
