9977ece013def6b5d50459b89cb30e1dd5a4d946
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-04-25 03:29:08 +0200

    Add ReportsController and make it authorized to only managers

---
+++ CRM/Controllers/ReportsController.cs
@@ -0,0 +1,21 @@
+﻿using CRM.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportsController : ControllerBase
+    {
+        private readonly IReportingService _reportingService;
+
+        public ReportsController(IReportingService reportingService)
+        {
+            _reportingService = reportingService;
+        }
+
+    }
+}
