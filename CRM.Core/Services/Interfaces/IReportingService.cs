aaee5434a547f5b891b56068cef411bbc6c404b5
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-04-25 03:15:20 +0200

    add IReportingService and its implementation to services container

---
+++ CRM.Core/Services/Interfaces/IReportingService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Core.Services.Interfaces
+{
+    public interface IReportingService
+    {
+    }
+}
