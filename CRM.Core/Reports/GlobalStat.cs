90ecc578c0a5f2d36bc95a49b412dc4d43bd3740
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-05-04 15:24:43 +0300

    add GlobalStateAsync to ReportingService

---
+++ CRM.Core/Reports/GlobalStat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM.Core.Reports
+{
+    public class GlobalStat
+    {
+        public ItemStat Customers { get; set; }
+        public ItemStat Actions { get; set; }
+        public ItemStat Deals { get; set; }
+        public ItemStat Revenue { get; set; }
+
+    }
+    public class ItemStat
+    {
+        public int Total { get; set; }
+        public int ThisWeek { get; set; }
+    }
+}
--- CRM.Core/Services/Implementations/ReportingService.cs
+++ CRM.Core/Services/Implementations/ReportingService.cs
@@ -57,5 +57,59 @@ namespace CRM.Core.Services.Implementations
             };
             return report;
         }
+        public async Task<GlobalStat> GlobalStatAsync()
+        {
+            var customers =await _unitOfWork.Customers.GetAllAsync();
+            var allCusotmers = customers.Count();
+            // return count of this week customers
+            var thisWeekCustomers = customers.Where(c => c.AdditionDate >= DateTime.Now.AddDays(-7)).Count();
+            
+            var deals = await _unitOfWork.Deals.GetAllAsync();
+            var allDeals = deals.Count();
+            var thisWeekDeals = deals.Where(d => d.DealDate >= DateTime.Now.AddDays(-7));
+            var thisWeekDealsCount = thisWeekDeals.Count();
+
+            var revenues = (int) deals.Sum(d => d.Price);
+            var thisWeekrevenues =(int) thisWeekDeals.Sum(d => d.Price);
+
+            var meetings = await _unitOfWork.Meetings.GetAllAsync();
+            var allMeetings = meetings.Count();
+            var thisWeekMeetings = meetings.Where(m=>m.MeetingDate>= DateTime.Now.AddDays(-7)).Count();
+
+            var calls = await _unitOfWork.Calls.GetAllAsync();
+            var allCalls = calls.Count();
+            var thisWeekCalls = calls.Where(c=>c.CallDate>= DateTime.Now.AddDays(-7)).Count();
+
+            var messages = await _unitOfWork.Messages.GetAllAsync();
+            var allMessages = messages.Count();
+            var thisWeekMessages = messages.Where(m=>m.MessageDate >= DateTime.Now.AddDays(-7)).Count();
+
+            var allActions = allCalls + allMessages + allMeetings + allDeals;
+            var thisWeekActions = thisWeekCalls + thisWeekMessages + thisWeekMeetings + thisWeekDealsCount;
+            return new GlobalStat
+            {
+                Customers =
+                {
+                    Total=allCusotmers,
+                    ThisWeek=thisWeekCustomers
+                },
+                Deals =
+                {
+                    Total= allDeals,
+                    ThisWeek= thisWeekDealsCount
+                },
+                Revenue =
+                {
+                    Total=revenues,
+                    ThisWeek=thisWeekrevenues
+                },
+                Actions =
+                {
+                    Total=allActions,
+                    ThisWeek=thisWeekActions
+                }
+            };
+            
+        }
     }
 }
