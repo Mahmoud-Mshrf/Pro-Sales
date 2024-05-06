8c37f8637c482419d669522ec6e0fdf77ba3f028
Mahmoud-Mshrf <mahmoudmshrf237@gmail.com>
2024-05-07 00:59:45 +0300

    deals-report and sources-report endpoints added to ReportsController

--- CRM/Controllers/ReportsController.cs
+++ CRM/Controllers/ReportsController.cs
@@ -12,7 +12,6 @@ namespace CRM.Controllers
     public class ReportsController : ControllerBase
     {
         private readonly IReportingService _reportingService;
-
         public ReportsController(IReportingService reportingService)
         {
             _reportingService = reportingService;
@@ -20,18 +19,7 @@ namespace CRM.Controllers
         [HttpGet("main-report")]
         public async Task<IActionResult> DailyReport(int page, int size, string? within)
         {
-            //if (within == "Daily" || within == "Monthly" || within == "Weekly")
-            //{
-            //    var result = await _reportingService.MainReport(page, size, within);
-            //    return Ok(result);
-            //}
-            //else
-            //{
-            //    var error = new List<string>();
-            //    error = ["Not a vaild time period"];
-            //    var errors = new { error };
-            //    return BadRequest(errors);
-            //}
+
            var result = await _reportingService.MainReport(page, size, within);
            return Ok(result);
             
@@ -45,23 +33,6 @@ namespace CRM.Controllers
         [HttpGet("sales-reprot/{id}")]
         public async Task<IActionResult> SalesReport(string id, string? within)
         {
-            //if (within == "Daily" || within == "Monthly" || within == "Weekly")
-            //{
-            //    var result = await _reportingService.SalesReport(id, within);
-            //    if (!result.IsSuccess)
-            //    {
-            //        var errors = new { result.Errors };
-            //        return BadRequest(errors);
-            //    }
-            //    return Ok(result);
-            //}
-            //else
-            //{
-            //    var error = new List<string>();
-            //    error = ["Not a vaild time period"];
-            //    var errors = new { error };
-            //    return BadRequest(errors);
-            //}
             var result = await _reportingService.SalesReport(id, within);
             if (!result.IsSuccess)
             {
@@ -71,5 +42,17 @@ namespace CRM.Controllers
             return Ok(result);
             
         }
+        //[HttpGet("deals-report")]
+        //public async Task<IActionResult> DealsReport(string? within)
+        //{
+        //    var result = await _reportingService.DealsReport(within);
+        //    return Ok(result);
+        //}
+        //[HttpGet("sources-report")]
+        //public async Task<IActionResult> SourcesReport(string? within)
+        //{
+        //    var result = await _reportingService.SourcesReport(within);
+        //    return Ok(result);
+        //}
     }
 }
